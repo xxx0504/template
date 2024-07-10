4de3d5477b9c362ae150ec096b6b31628f08f2b3
Aiden <xxx541122@163.com>
2024-07-10 15:11:40 +0800

    feat: 删除（20240710）

--- src/service/api/goods/goods.js
+++
@@ -1,9 +0,0 @@
-import http from '@/service/http';
-export const goodsApiUrl = {
-    queryGoodsList: '/activity-invite/queryActivityInviteRule',
-};
-export const goodsApi = {
-    queryGoodsList: async function (params) {
-        return await http.get(goodsApiUrl.queryGoodsList, params);
-    },
-};
